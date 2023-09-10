40e736eea0c0cb75ac98fa774502674d7b23eae6
kawthar_makarem <kawtharmakarem@gmail.com>
2023-09-10 21:10:04 +0300

    first update

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_application_test/data/network/failure.dart';
+
 enum DataSource {
   SUCCESS,
   NO_CONTENT,
@@ -14,24 +16,81 @@ enum DataSource {
   NO_INTERNET_CONNECTION,
 }
 
-class ResponseCode {
-  static const String SUCCESS = "success";//success with data
-  static const String BAD_REQUEST = "success"; //success with no data
-  static const String NO_CONTENT = "Bad request,Try again later";  //failure Api rejected request
-  static const String UNAUTHORISED = "User is unauthoriSed , Try again later";  //failure ,user is not authorized
-  static const String INTERNAL_SERVER_ERROR = "Something went wrong ,Try again later"; //faliure crache in server
-    static const String FORBIDDEN = "Forbidden , Try again later"; // failure api rejected request
-
-//local status code
-  static const String CONNECT_TIMEOUT = "Time out error ,Try again later";
-  static const String CANCEL = "Request was cancelled ,Try again later";
-  static const String RECIEVE_TIMEOUT = "Time out error ,Try again later";
-  static const String SEND_TIMEOUT = "Time out error ,Try again later";
-  static const String CACHE_ERROR = "Cache error ,Try again later";
-  static const String NO_INTERNET_CONNECTION = "Please check your internet connection";
-    static const String UNKNOWN = "Something went wrong ,Try again later";
-
+extension DataSourceExtension on DataSource{
+  Failure getFailure(){
+    switch(this){
+      
+      case DataSource.SUCCESS:
+      return Failure(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+      case DataSource.NO_CONTENT:
+      return Failure(ResponseCode.NO_CONTENT, ResponseMessage.NO_CONTENT);
+      case DataSource.BAD_REQUEST:
+      return Failure(ResponseCode.BAD_REQUEST, ResponseMessage.BAD_REQUEST);
+      case DataSource.FORBIDDEN:
+      return Failure(ResponseCode.FORBIDDEN, ResponseMessage.FORBIDDEN);
+      case DataSource.UNAUTHORISED:
+        return Failure(ResponseCode.UNAUTHORISED, ResponseMessage.UNAUTORISED);
+      case DataSource.NOT_FOUND:
+      return Failure(ResponseCode.NOT_FOUND,ResponseMessage.NOT_FOUND);
+      case DataSource.INTERNAL_SERVER_ERROR:
+        return Failure(ResponseCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
+      case DataSource.CONNECT_TIMEOUT:
+        return Failure(ResponseCode.CONNECT_TIMEOUT, ResponseMessage.CONNECT_TIMEOUT);
+      case DataSource.CANCEL:
+        return Failure(ResponseCode.CANCEL, ResponseMessage.CANCEL);
+      case DataSource.RECIEVE_TIMEOUT:
+        return Failure(ResponseCode.RECIEVE_TIMEOUT, ResponseMessage.CONNECT_TIMEOUT);
+      case DataSource.SEND_TIMEOUT:
+        return Failure(ResponseCode.SEND_TIMEOUT, ResponseMessage.SEND_TIMEOUT);
+      case DataSource.CACHE_ERROR:
+        return Failure(ResponseCode.CACHE_ERROR, ResponseMessage.CACHE_ERROR);
+      case DataSource.NO_INTERNET_CONNECTION:
+        return Failure(ResponseCode.NO_INTERNET_CONNECTION, ResponseMessage.NO_INTERNET_CONNECTION);
+    }
+  }
 }
-class ResponseMessage{
 
+class ResponseCode {
+  static const int SUCCESS = 200; // success with data
+  static const int NO_CONTENT = 201; // success with no data (no content)
+  static const int BAD_REQUEST = 400; // failure, API rejected request
+  static const int UNAUTHORISED = 401; // failure, user is not authorised
+  static const int FORBIDDEN = 403; //  failure, API rejected request
+  static const int INTERNAL_SERVER_ERROR = 500; // failure, crash in server side
+  static const int NOT_FOUND = 404; // failure, not found
+
+  // local status code
+  static const int CONNECT_TIMEOUT = -1;
+  static const int CANCEL = -2;
+  static const int RECIEVE_TIMEOUT = -3;
+  static const int SEND_TIMEOUT = -4;
+  static const int CACHE_ERROR = -5;
+  static const int NO_INTERNET_CONNECTION = -6;
+  static const int UNKNOWN = -7;
+}
+
+class ResponseMessage {
+  static const String SUCCESS = "success"; // success with data
+  static const String NO_CONTENT =
+      "success"; // success with no data (no content)
+  static const String BAD_REQUEST =
+      "Bad request, Try again later"; // failure, API rejected request
+  static const String UNAUTORISED =
+      "User is unauthorised, Try again later"; // failure, user is not authorised
+  static const String FORBIDDEN =
+      "Forbidden request, Try again later"; //  failure, API rejected request
+  static const String INTERNAL_SERVER_ERROR =
+      "Some thing went wrong, Try again later"; // failure, crash in server side
+  static const String NOT_FOUND =
+      "Some thing went wrong, Try again later"; // failure, crash in server side
+
+  // local status code
+  static const String CONNECT_TIMEOUT = "Time out error, Try again later";
+  static const String CANCEL = "Request was cancelled, Try again later";
+  static const String RECIEVE_TIMEOUT = "Time out error, Try again later";
+  static const String SEND_TIMEOUT = "Time out error, Try again later";
+  static const String CACHE_ERROR = "Cache error, Try again later";
+  static const String NO_INTERNET_CONNECTION =
+      "Please check your internet connection";
+  static const String UNKNOWN = "Some thing went wrong, Try again later";
 }
